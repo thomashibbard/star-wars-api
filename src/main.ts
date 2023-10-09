8e8bf8302e429e7cbcffe4a6e82f26018f5374cc
Thomas Hibbard <thomas@vermill.io>
2023-10-09 12:56:08 -0700

    uses port exposed through process

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FastifyAdapter } from '@nestjs/platform-fastify';
 import { ValidationPipe } from '@nestjs/common';
-
+const port = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new FastifyAdapter(), {
     cors: true,
   });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
