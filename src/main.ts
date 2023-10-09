61b45be96c3791c7cd7793416f6ceac856a57cdf
Thomas Hibbard <thomas@vermill.io>
2023-10-09 13:42:01 -0700

    adds address

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FastifyAdapter } from '@nestjs/platform-fastify';
 import { ValidationPipe } from '@nestjs/common';
-console.log(`Process port`, process.env.PORT);
-const port = process.env.PORT || 3000;
+const address = (process.env.ADDRESS = '0.0.0.0');
+const port = process.env.PORT || 5000;
 async function bootstrap() {
+  console.log(`Bootrap ${address}@${port}`);
   const app = await NestFactory.create(AppModule, new FastifyAdapter(), {
     cors: true,
   });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(port);
+  await app.listen(port, address);
 }
 bootstrap();
