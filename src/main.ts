d3fc7947be32518c2f0ae3531e7710cea1cf4f88
Thomas Hibbard <thomas@vermill.io>
2023-10-09 13:18:33 -0700

    restores port

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { FastifyAdapter } from '@nestjs/platform-fastify';
 import { ValidationPipe } from '@nestjs/common';
 console.log(`Process port`, process.env.PORT);
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new FastifyAdapter(), {
     cors: true,
